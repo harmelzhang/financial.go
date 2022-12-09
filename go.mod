85b5104c62b209d5d7695ed4f972cb475811524a
zhangpeng <harmel@163.com>
2022-12-09 18:29:26 +0800

    切换 xls 读取库解决丢失数据问题

--- go.mod
+++ go.mod
@@ -4,15 +4,15 @@ go 1.19
 
 require (
 	github.com/antchfx/htmlquery v1.2.5
-	github.com/extrame/xls v0.0.1
 	github.com/go-sql-driver/mysql v1.7.0
 	github.com/mozillazg/go-pinyin v0.19.0
+	github.com/shakinm/xlsReader v0.9.12
 	golang.org/x/net v0.2.0
 )
 
 require (
 	github.com/antchfx/xpath v1.2.1 // indirect
-	github.com/extrame/ole2 v0.0.0-20160812065207-d69429661ad7 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/metakeule/fmtdate v1.1.2 // indirect
 	golang.org/x/text v0.4.0 // indirect
 )
--- utils/xls/xls.go
+++ utils/xls/xls.go
@@ -2,29 +2,39 @@ package xls
 
 import (
 	"bytes"
-	"github.com/extrame/xls"
+	"fmt"
+	"github.com/shakinm/xlsReader/xls"
 	"log"
 )
 
 // ReadXls 读取 XLS 数据
 func ReadXls(data []byte, sheetIndex int, skipRow int) [][]string {
-	wb, err := xls.OpenReader(bytes.NewReader(data), "UTF-8")
+	wb, err := xls.OpenReader(bytes.NewReader(data))
 	if err != nil {
-		log.Fatalf("执行出错 : %s", err)
+		log.Fatalf("读取 Xls 出错 : %s", err)
 	}
 
-	sheet := wb.GetSheet(sheetIndex)
+	sheet, err := wb.GetSheet(sheetIndex)
+	if err != nil {
+		log.Fatalf("读取 Sheet 出错 : %s", err)
+	}
+
 	sheetData := make([][]string, 0)
-	for i := 0; i < int(sheet.MaxRow); i++ {
+	for i := 0; i < sheet.GetNumberRows(); i++ {
 		if i == skipRow {
 			continue
 		}
-		row := sheet.Row(i)
+
 		rowData := make([]string, 0)
-		for j := 0; j < row.LastCol(); j++ {
-			colData := row.Col(j)
-			rowData = append(rowData, colData)
+		row, err := sheet.GetRow(i)
+		if err != nil {
+			log.Fatalf("读取 Row 出错 : %s", err)
 		}
+
+		for _, cellData := range row.GetCols() {
+			rowData = append(rowData, cellData.GetString())
+		}
+		fmt.Println("ROW: ", rowData)
 		sheetData = append(sheetData, rowData)
 	}
 
