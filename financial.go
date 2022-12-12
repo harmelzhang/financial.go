29669f1e7f8aaebe1aa6b30300ce3015947b240f
zhangpeng <harmel@163.com>
2022-12-12 17:13:38 +0800

    fix：断点续爬时间差值问题

--- financial.go
+++ financial.go
@@ -27,7 +27,7 @@ func main() {
 
 	// 如果上次成功了，判断时间是否大于配置天数
 	if progress.Done {
-		if progress.Time >= config.TaskIntervalDay*24*3600 {
+		if time.Now().Unix()-progress.Time >= config.TaskIntervalDay*24*3600 {
 			progress = models.NewProgress()
 		} else {
 			log.Printf("任务结束 : 离上次任务成功结束时间小于%d天", config.TaskIntervalDay)
